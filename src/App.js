bb30e8de7f4a11d034b3b552eab542db1488ec4d
VilleK <101732127+VilleKoiv@users.noreply.github.com>
2022-08-29 20:12:16 +0300

    BMI laskuri Reactilla

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [height,setHeight] = useState(0)
+  const [weight,setWeight] = useState(0)
+  const [summary,setSummary] = useState(0)
+  
+  function calculate(e) {
+    const conversion = weight / (height * height)
+    setSummary(conversion)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h3>Calculating body mass index</h3>
+      <div>
+        <label>Height</label>
+        <input type="number" value={height} onChange={e => setHeight(e.target.value)}/>
+      </div>
+
+      <div>
+        <label>Weight</label>
+        <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/> <br/>
+      
+      <label>BMI </label>
+      <output>{summary}</output>
+      </div>
+
+      <button type="button" onClick={calculate}>Calculate</button>
+    </form>
   );
 }
 
